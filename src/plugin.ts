18db566452c3833cac187c6aeae573301ce0d79e
Edward Ruchevits <ruchevits@gmail.com>
2020-08-04 21:58:57 +0300

    fix require path typo

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,8 +3,7 @@ import * as http from 'http';
 import * as http2 from 'http2';
 import * as https from 'https';
 
-import { MapperOptions } from '../dist/mapper';
-import { CaseFormat, Formatter, mapper } from './mapper';
+import { CaseFormat, Formatter, mapper, MapperOptions } from './mapper';
 
 export type HttpServer = http.Server | https.Server | http2.Http2Server | http2.Http2SecureServer;
 export type HttpRequest = http.IncomingMessage | http2.Http2ServerRequest;
